e5f89fe9398d542956f5f840c8ca344f24cf303c
Andrea Parthemore <aparthemore@vistahigherlearning.com>
2015-01-24 14:09:37 -0500

    First commit ever.

---
+++ erc.el
@@ -0,0 +1,112 @@
+(require 'erc)
+(require 'erc-log)
+(require 'erc-notify)
+(require 'erc-spelling)
+(require 'erc-autoaway)
+(require 'erc-services)
+(require 'notifications)
+
+;; Notify the user of private messages
+;; (defun erc-private-message-notify (proc parsed)
+;;   (let ((nick (car (erc-parse-user (erc-response.sender parsed))))
+;;         (target (car (erc-response.command-args parsed)))
+;;         (msg (erc-response.contents parsed)))
+;;     (notifications-notify :title nick :body msg)))
+
+;; (add-hook 'erc-server-PRIVMSG-functions 'erc-private-message-notify)
+
+;; Notify the user when their nick is mentioned
+;; (defun erc-nick-mentioned-notify (match-type nick message)
+;;   "Shows a notification when user's nick is mentioned."
+;;   (when (and (not (posix-string-match "^\\** *Users on #" message))
+;;              (eq math-type 'current-nick))
+;;     (notifications-notify
+;;      :title nick
+;;      :body message
+;;      :app-icon "/usr/share/notify-osd/icons/gnome/scalable/status/notification-message-im.svg"
+;;      :urgency 'low)))
+
+;; (add-hook 'erc-text-matched-hook 'erc-nick-mentioned-notify)
+
+;; Nickname
+(setq erc-nick "rainingfish")
+
+;; Don't steal focus 
+(setq erc-join-buffer 'bury)
+
+;; Interpret mIRC-style color commands in IRC chats
+(setq erc-interpret-mirc-color t)
+
+;; The following are commented out by default, but users of other
+;; non-Emacs IRC clients might find them useful.
+;; Kill buffers for channels after /part
+(setq erc-kill-buffer-on-part t)
+;; Kill buffers for private queries after quitting the server
+(setq erc-kill-queries-on-quit t)
+;; Kill buffers for server messages after quitting the server
+(setq erc-kill-server-buffer-on-quit t)
+
+;; open query buffers in the current window
+(setq erc-query-display 'buffer)
+
+;; exclude boring stuff from tracking
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                "324" "329" "332" "333" "353" "477"))
+
+;; logging
+(setq erc-log-channels-directory "~/.erc/logs/")
+
+(if (not (file-exists-p erc-log-channels-directory))
+    (mkdir erc-log-channels-directory t))
+
+(setq erc-save-buffer-on-part t)
+
+;; truncate long irc buffers
+(erc-truncate-mode +1)
+
+;; enable spell checking
+(erc-spelling-mode 0)
+
+;; utf-8 always and forever
+(setq erc-server-coding-system '(utf-8 . utf-8))
+
+;; Auto-join channels
+;; (erc-autojoin-mode t)
+;; (setq erc-autojoin-channels-alist
+;;       '(("freenode.net" "#libre.fm" "#guile" "#guix"
+;;          "#gnu" "#emacs" "#scheme" "#vidyadev" "#libreplanet-gaming"
+;;          "#lispgames" "#guile-2d")))
+
+;; Secret password file
+(load "~/.emacs.d/.ercpasswords")
+
+;; Stuff to make authenticating with Rizon work
+;; (erc-services-mode t)
+;; (setq erc-prompt-for-nickserv-password nil)
+;; (setq erc-nickserv-identify-mode 'autodetect)
+;; (setq erc-nickserv-passwords
+;;       `((Rizon (("rainingfish" . ,rizon-password)))))
+
+;; Start and stop erc
+(defun start-irc ()
+  "Connect to IRC."
+  (interactive)
+  (erc :server "irc.freenode.net"
+       :port 6667
+       :nick erc-nick
+       :password freenode-password))
+
+(defun filter-erc-server-buffers ()
+  (delq nil
+        (mapcar
+         (lambda (x) (and (erc-server-buffer-p x) x))
+         (buffer-list))))
+
+(defun stop-irc ()
+  "Disconnects from all irc servers"
+  (interactive)
+  (dolist (buffer (filter-erc-server-buffers))
+    (message "Server buffer: %s" (buffer-name buffer))
+    (with-current-buffer buffer
+      (erc-quit-server "Later"))))
---
+++ init.el
@@ -0,0 +1,161 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Helpful keyboard tweak to make things easier on OS X.
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+;; No splash screen.
+(setq inhibit-startup-message t)
+
+;; Visual bell instead of beeping
+(setq ring-bell-function 'ignore)
+
+;; Interactively do shit!
+(require 'ido)
+(ido-mode t)
+;;(ido-ubiquitous-mode t)
+
+;; Handy line-related thingsgX
+(global-linum-mode t)
+(column-number-mode t)
+(global-hl-line-mode t)
+
+;; Remember open buffers for next session.
+;; (desktop-save-mode t)
+
+;; Highlight matching parens, use rainbow delimiters.
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+;;(global-rainbow-delimiters-mode t) this isn't working.
+(show-paren-mode t)
+
+;; Auto-completion is sick!
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+;; Tabs and alignment
+(setq-default indent-tabs-mode nil)
+(setq indent-tabs-mode nil)
+;(setq c-basic-offset 4)
+;(setq c-basic-indent 2)
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+(setq js2-basic-offset 2)
+;;(electric-indent-mode t)
+
+;; Javascript
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; Handy functions courtesy of whattheemacs.d
+(defun open-line-below ()
+  (interactive)
+  (if (eolp)
+      (newline)
+    (end-of-line)
+    (newline))
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+;;keybindings
+(global-set-key (kbd "<C-return>") 'open-line-below)
+(global-set-key (kbd "<C-M-return>") 'open-line-above)
+(global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-x g") 'goto-line)
+;; No more minimizing Emacs by accident.
+(global-unset-key (kbd "C-z"))
+
+;;default open file dir
+(setq default-directory "~/src/m3/")
+
+;;theme
+(load-theme 'twilight-anti-bright t)
+
+
+;;revert all buffers that are visiting a file
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files.") )
+
+;;(setq scss-output-directory "../../maestro_activity_engine/activities_general/")
+(setq scss-output-directory "../maestro_activity_engine/")
+;;(setq scss-output-directory "../../activities/")
+;;(setq scss-output-directory "../")
+;;(setq scss-compile-at-save t)
+
+
+;; Make rinari jump to/from javascript source files and specs.
+;; C-c ; f j
+;;(require 'rinari)
+;; (setcdr (assoc 'javascript rinari-jump-schema)
+;;         '("j"
+;;           (("app/assets/javascripts/\\1.js" . "spec/javascripts/\\1_spec.js")
+;;            ("spec/javascripts/\\1_spec.js"  . "app/assets/javascripts/\\1.js")
+;;            (t . "spec/javascripts/.*")
+;;            (t . "app/javascripts/.*"))
+;;           t))
+;; (rinari-apply-jump-schema rinari-jump-schema)
+
+
+;;add newline at end of file
+(setq require-final-newline t)
+
+
+;;magit status
+(global-set-key (kbd "C-c g") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;ruby-block
+;; (require 'ruby-block)
+;;   (setq ruby-block-delay 0)
+;;   (setq ruby-block-highlight-toggle t)
+;;   (ruby-block-mode t)
+
+
+;;icicles 
+;;(require 'icicles)
+
+(load "~/.emacs.d/erc")
+
+;;don't show toolbar
+(tool-bar-mode -1)
+
+
+;;Web-mode customizations
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
+;;ruby block
+(require 'ruby-block)
+(ruby-block-mode t)
+
+;;Because the OS Gui app environment
+;;does not have anything from .bashrc
+;;(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
