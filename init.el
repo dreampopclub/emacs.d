7012b0b4144d56ec82f591e6686e070eecd1285c
Andrea Parthemore <aparthemore@vistahigherlearning.com>
2015-04-09 12:33:56 -0400

    Remove white space on save.

--- init.el
+++ init.el
@@ -206,3 +206,13 @@
 ;;(nyan-mode +1)
 
 
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+;; Various superfluous white-space. Just say no.
+(add-hook 'before-save-hook 'cleanup-buffer-safe)
